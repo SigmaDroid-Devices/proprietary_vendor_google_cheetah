33180e9bb624f7e2c74f9cafa1e5babc32b77203
Matt Filetto <matt.filetto@gmail.com>
2024-03-07 16:18:48 -0800

    Drop duplicate DeviceIntelligenceNetworkPrebuilt
    * We track this in vendor/gms
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- cheetah-vendor.mk
+++ cheetah-vendor.mk
@@ -94,7 +94,6 @@ PRODUCT_PACKAGES += \
     ConnMO \
     DCMO \
     DMService \
-    DeviceIntelligenceNetworkPrebuilt \
     DevicePersonalizationPrebuiltPixel2022 \
     DiagMon \
     EuiccGoogle \
